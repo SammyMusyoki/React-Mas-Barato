7a77564354153121684bebd42125710e0973d52a
sammy musyoki <75688249+SammyMusyoki@users.noreply.github.com>
2023-03-04 12:41:03 +0300

    Navbar-toggle-update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,37 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Link, NavLink} from 'react-router-dom'
 import {links} from '../data'
 import {GoThreeBars} from 'react-icons/go'
+import {AiOutlineClose} from 'react-icons/ai'
 
 
 import './navbar.css'
-import Logo from '../images/logo.png'
+import Logo from '../images/Mas Barato.svg'
 
 const Navbar = () => {
+    const [isNavShowing, setIsNavShowing] = useState(false);
   return (
     <nav>
-        <div className='container nav_container'>
+        <div className='container nav__container'>
             <Link to="/" className='logo'>
                 <img src={Logo} alt="Nav Logo"></img>
             </Link>
-            <ul className='nav__links'>
+            <ul className={`nav__links ${isNavShowing ? 'show__nav' : 'hide__nav'}` }>
                 {
                     links.map(({name, path}, index) => {
                         return(
-                            <li>
-                                <NavLink to={path}>{name}</NavLink>
+                            <li key={index}>
+                                <NavLink to={path} className={({isActive}) => isActive ? 'active-nav' : ''}>{name}</NavLink>
                             </li>
                         )
                     })
                 }
             </ul>
-            <button className='nav__toggle-btn'>
-                <GoThreeBars/>
+            <button className='nav__toggle-btn' onClick={() => setIsNavShowing
+                (!isNavShowing)}>
+                {
+                    isNavShowing ? <AiOutlineClose/> : <GoThreeBars/>
+                }
             </button>
         </div>
     </nav>
